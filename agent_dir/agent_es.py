12145d3d032997a45de0e49fc0eb18c39515d915
ctinray <b03902072@ntu.edu.tw>
2017-12-01 11:38:48 +0800

    parallelize evaluation and change env to mountaincar

--- agent_dir/agent_es.py
+++ agent_dir/agent_es.py
@@ -1,13 +1,16 @@
+import copy
+import multiprocessing
 from es.simple_es import ES
 from tensorboardX import SummaryWriter
 import numpy as np
+
 
 class Agent_ES:
     def __init__(self, args, env, solve):
         # environment
         self.env = env
         self.solve = solve
-        # solver 
+        # solver
         self.num_params = args.num_params
         self.npop = args.npop
         self.solver = ES(args.num_params,
@@ -16,39 +19,46 @@ class Agent_ES:
         self.writer = SummaryWriter()
         self.num_timesteps = args.num_timesteps
         self.display_freq = args.display_freq
-    
-    def fitness(self, weight):
+
+        # parallelism args
+        self.n_workers = args.n_workers or multiprocessing.cpu_count()
+
+    @staticmethod
+    def fitness(args):
+        env, weight = args
         total_reward = 0.0
         num_run = 100
         for t in range(num_run):
-            observation = self.env.reset()
+            observation = env.reset()
             done = False
             while(not done):
-                action = 1 if np.dot(weight, observation) > 0 else 0
-            
-                observation, reward, done, info = self.env.step(action)
+                action = 2 if np.dot(weight, observation) > 0 else 0
+
+                observation, reward, done, info = env.step(action)
                 total_reward += reward
         return total_reward / num_run
-    
+
     def train(self):
         for i_timestep in range(self.num_timesteps):
             solutions = self.solver.ask()
-            rewards = []
-            for weight in solutions:
-                rewards.append(self.fitness(weight))
+
+            with multiprocessing.Pool(self.n_workers) as p:
+                envs = [copy.deepcopy(self.env) for i in range(len(solutions))]
+                rewards = p.map(self.fitness, zip(envs, solutions))
+                rewards = list(rewards)
             rewards = np.array(rewards)
             self.solver.tell(rewards)
 
             self.writer.add_scalar('best reward', np.max(rewards), i_timestep)
             self.writer.add_scalar('mean reward', np.mean(rewards), i_timestep)
             if i_timestep % self.display_freq == 0:
-                print('Step: %d | Best Reward %f | Avg Reward %f | Weight %s'%
-                       (i_timestep, np.max(rewards), np.mean(rewards), self.solver.solution))
-            
+                print('Step: %d | Best Reward %f | Avg Reward %f | Weight %s' %
+                      (i_timestep, np.max(rewards), np.mean(rewards), self.solver.solution))
+
             if np.max(rewards) > self.solve[0]:
                 print('Solve the environment. Stop training')
-                print('# fitness function:%d'%(
-                      (i_timestep+1) * self.npop
-                ))
+                print('# fitness function:%d' % (
+                      (i_timestep + 1) * self.npop
+                      ))
                 break
         self.writer.close()
--- main_es.py
+++ main_es.py
@@ -8,19 +8,19 @@ def parse():
     parser.add_argument('--env_name',type=str,default='PongNoFrameskip-v4' ,help='whether render environment')
 
     # Training hyperparameters
-    parser.add_argument('--num_params',type=int,default=4,help='number of parameters')
+    parser.add_argument('--num_params',type=int,default=2,help='number of parameters')
     parser.add_argument('--npop',type=int,default=50,help='population size')
     parser.add_argument('--num_timesteps',type=int,default=50,help='number of training steps')
     parser.add_argument('--display_freq',type=int,default=1,help='display training status every n episodes')
     parser.add_argument('--save_freq',type=int,default=200,help='save model every n steps')
-    
+    parser.add_argument('--n_workers',type=int,default=None,help='Number of threads to do eval.')
     
 
     args = parser.parse_args()
     return args
 
 def run(args):
-    env = gym.make('CartPole-v0')
+    env = gym.make('MountainCar-v0')
     solve = (195, 100) # we solve cartpole when getting reward of 195 over 100 episode
     agent = Agent_ES(args, env, solve)
     agent.train()
