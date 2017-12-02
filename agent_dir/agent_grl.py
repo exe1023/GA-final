9dbeea4e3343d6c0ba4d8071d39350221cf93729
ctinray <b03902072@ntu.edu.tw>
2017-12-02 23:00:46 +0800

    finish writing AgentGRL.crossover

--- agent_dir/agent_grl.py
+++ agent_dir/agent_grl.py
@@ -1,13 +1,13 @@
 import copy
 import itertools
-import torch
+import numpy as np
+from agent_base import AgentBase
 
 
 class AgentGRL():
     """ A deep Q network agent optimize with genetic algorithm.
 
     Args:
-        env (gym): Env made with gym.
         base_agent (Agent): Any RL agent.
         agent_clf (Classifier): Any classifier that map observation to
             choice of agent. Used in process of crossover (distillation).
@@ -16,20 +16,16 @@ class AgentGRL():
         distil_steps (int): Number of steps to run to do distillation.
         use_cuda (bool): Wheather or not use CUDA (GPU).
     """
-    def __init__(self, env,
+    def __init__(self,
                  base_agent,
                  agent_clf,
                  population_size=8,
                  n_generations=100,
-                 distil_steps=10000,
-                 use_cuda=None):
-        self.env = env
+                 distil_steps=10000):
+        self.base_agent = base_agent
         self.agent_clf = agent_clf
         self.n_generations = n_generations
         self.distil_steps = distil_steps
-        if use_cuda is None:
-            use_cuda = torch.cuda.is_available()
-        self.use_cuda = use_cuda
 
         # init GA population with random chromosomes
         self.population = [copy.deepcopy(base_agent)
@@ -87,6 +83,51 @@ class AgentGRL():
         Returns:
             chrom: Child chromosome.
         """
-        clfs = []
-        pass
-    
+        # get experience from the parent
+        p0_exp = parent[0].get_experience()
+        p1_exp = parent[1].get_experience()
+
+        # init classifier
+        clf = copy.deepcopy(self.agent_clf)
+
+        # make training data for the classifier
+        x = np.concatenate([p0_exp, p1_exp], axis=0)
+        y = np.concatenate([np.zeros(p0_exp.shape[0]),
+                            np.ones(p1_exp.shape[1])],
+                           axis=0)
+
+        # train the classifier
+        clf.fit(x, y)
+
+        # build hierarchical agent
+        hier_agent = _HierarchicalAgent(clf, parent)
+
+        # init child
+        child = copy.deepcopy(self.base_agent)
+
+        # doing distillation
+        child.learn(hier_agent)
+
+
+class _HierarchicalAgent(AgentBase):
+    """A hierarchical agent.
+
+    Args:
+        clf (classifier): A trained classifier that decides
+            which agent to use.
+        agents (tuple of Agent): Agents.
+    """
+    def __init__(self, clf, agents):
+        super(_HierarchicalAgent).__init__(self)
+        self.clf = clf
+        self.agents = agents
+
+    def make_action(self, observation):
+        agent_proba = self.clf.predict_proba(observation)
+        return type(self.agents[0]) \
+            .jointly_make_action(observation, self.agents, agent_proba)
+
+    def get_action_raw(self, observation):
+        agent_proba = self.clf.predict_proba(observation)
+        return type(self.agents[0]) \
+            .jointly_get_action_raw(observation, self.agents, agent_proba)
