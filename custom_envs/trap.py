9df51237e309d22de2700b66b2564737eeb354ae
Lasfolt <b03902071@ntu.edu.tw>
2017-12-30 15:37:53 +0800

    update trap

--- custom_envs/trap.py
+++ custom_envs/trap.py
@@ -13,15 +13,19 @@ class MKTrap:
         self.problem_size = m * k
         self.m = m
         self.k = k
-        
+        # which bits should form the trap?
+        self.traps = random.sample(range(self.problem_size), 
+                                   self.problem_size)
+        # what is the correct BB?
+        self.correct = [random.randint(0, 1) for _ in range(k)]
+
         self.observation_space = Space(n=None, shape=(self.problem_size, ))
         self.action_space = Space(n=2, shape=None)
+        print('Correct BB:', self.correct)
         print('Best Reward:', 2 * m)
 
     
     def reset(self):
-        self.traps = random.sample(range(self.problem_size), 
-                                   self.problem_size)
         self.state = [2 for _ in range(self.problem_size)]
         self.idx = 0
         self.done = False
@@ -45,16 +49,16 @@ class MKTrap:
         '''
         The best BB is 111...1 and its reward is 2
         The deceptive BB is 000...0 and its reward is 1
-        Reward of other BBs is (# of 0) * (1/k)
+        Reward of other BBs is (# of incorrect) * (1/k)
         '''
         if not self.done:
             return 0
         
         reward = 0
         for i in range(0, self.problem_size, self.k):
-            trap = self.traps[i: i + self.k]
-            num_0 = 0
-            for t in trap:
-                num_0 += 1 if self.state[t] == 0 else 0
-            reward += 2 if num_0 == 0 else num_0 * (1/self.k)
+            trap = sorted(self.traps[i: i + self.k])
+            num_fail = 0
+            for i, t in enumerate(trap):
+                num_fail += 1 if self.state[t] != self.correct[i] else 0
+            reward += 2 if num_fail == 0 else num_fail * (1/self.k)
         return reward
